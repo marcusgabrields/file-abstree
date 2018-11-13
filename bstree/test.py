6078c8c2d21d42f59657950a02212ad78cbef110
Gabriel Dantas <marcusgabriel.ds@gmail.com>
2018-11-13 10:00:13 -0300

    Firt test for check permissions

---
+++ bstree/test.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+
+class FileBasicsTestCase(unittest.TestCase):
+
+    FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+    FILE_NAME = FILE_PATH + '/FileBasicsTestCase.bin'.lower()
+
+    def setUp(self):
+        self.f = open(self.FILE_NAME, 'wb')
+    
+    def tearDown(self):
+        self.f.close()
+        os.remove(self.FILE_NAME)
+    
+    def test_can_create_a_fili(self):
+        self.assertTrue(os.path.exists(self.FILE_NAME))
+        self.assertTrue(os.path.isfile(self.FILE_NAME))
+    
+
+if __name__ == '__main__':
+    unittest.main()
