e8438886e15a8af4da2dfc193b0e0c1826c846c5
Gabriel Dantas <marcusgabriel.ds@gmail.com>
2018-11-14 00:14:21 -0300

    Refactoring bstree

--- bstree/bstree.py
+++ bstree/bstree.py
@@ -11,11 +11,26 @@ class BSTree:
         self.size = 0
     
     def insert(self, data):
-        if not self.root:
-            self._file.seek(0, 2)
-            new_node = struct.pack('{}s'.format(self.STRING_SIZE), data.encode())
-            self._file.write(new_node)
+        if self.root == None:
+            self.root = self.__pack(0, data)
             self.size = self.size + 1
+        else:
+            pass
+        
+    def _insert(data, key):
+        pass
     
+    def __unpack(self, key):
+        self._file.seek(key)
+        node = struct.unpack('10s i i i', self._file.read(24))
+        node = (node[0].decode().strip('\x00'), node[1], node[2], node[3])
+        return node
+    
+    def __pack(self, key, data, left_child=-1, right_child=-1, deleted=0):
+        new_node_key = self._file.seek(key)
+        new_node = struct.pack('10s i i i'.format(self.STRING_SIZE), data.encode(), left_child, right_child, deleted)
+        self._file.write(new_node)
+        return new_node_key
+
     def __len__(self):
         return self.size
